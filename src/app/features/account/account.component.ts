e1f451a36a5c8acd772333fbc9df7acf0dc57480
William <williamcondori50@gmail.com>
2024-11-27 17:03:43 -0500

    refact: changed design

--- src/app/features/account/account.component.ts
+++ src/app/features/account/account.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-account',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
-
+  profileIsActived = signal(true);
+  pruebita = true;
+  profileActived(): void {
+    this.profileIsActived.set(!this.profileIsActived())
+  }
 }
