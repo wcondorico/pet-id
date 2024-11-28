ad90096d1d6ac605a7cced0d4d39fcb0aa4f76a7
William <williamcondori50@gmail.com>
2024-11-27 22:08:43 -0500

    refactor: abstraction card-button card-option

--- src/app/core/components/card/card-options/card-options.component.ts
+++ src/app/core/components/card/card-options/card-options.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
+import { SubtitleComponent } from '../../subtitle/subtitle.component';
 
 @Component({
   selector: 'app-card-options',
-  imports: [],
+  imports: [SubtitleComponent],
   templateUrl: './card-options.component.html',
   styleUrl: './card-options.component.scss'
 })
 export class CardOptionsComponent {
   @Input() items!: {icon:string; text:string}[];
+  title = input.required()
 }
