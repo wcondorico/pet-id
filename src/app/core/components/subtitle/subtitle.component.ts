416b98e3a28c9266a02fdf176943081ad1d70ba8
William <williamcondori50@gmail.com>
2024-11-27 21:37:21 -0500

    refact: card-button changes element

---
+++ src/app/core/components/subtitle/subtitle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subtitle',
+  imports: [],
+  templateUrl: './subtitle.component.html',
+  styleUrl: './subtitle.component.scss'
+})
+export class SubtitleComponent {
+
+}
