514031b34a5e0543f8462c7619791a0535c611f6
William <williamcondori50@gmail.com>
2024-11-27 21:18:11 -0500

    refact: add card-option abstraction

--- src/app/core/components/card/card-options/card-options.component.ts
+++ src/app/core/components/card/card-options/card-options.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-options',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './card-options.component.scss'
 })
 export class CardOptionsComponent {
-
+  @Input() items!: {icon:string; text:string}[];
 }
--- src/app/features/account/views/home/home.component.ts
+++ src/app/features/account/views/home/home.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TitleComponent } from '../../../../core/components/title/title.component';
 import { CardButtonComponent } from '../../../../core/components/card/card-button/card-button.component';
+import { CardOptionsComponent } from '../../../../core/components/card/card-options/card-options.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TitleComponent,CardButtonComponent],
+  imports: [TitleComponent, CardButtonComponent, CardOptionsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  list = signal([
+    {icon:'pets', text:'Mis mascotas'},
+    {icon:'pets', text:'Anuncios'},
+    {icon:'pets', text:'Adopción'},
+    {icon:'pets', text:'Eliminar cuenta'}
+  ]);
 }
