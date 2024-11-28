429867fb0fa4f48660fc5781221f5265635386f3
William <williamcondori50@gmail.com>
2024-11-27 19:01:34 -0500

    refact: button header design
    refact: h1 design

---
+++ src/app/core/components/card/card-options/card-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-options',
+  imports: [],
+  templateUrl: './card-options.component.html',
+  styleUrl: './card-options.component.scss'
+})
+export class CardOptionsComponent {
+
+}
