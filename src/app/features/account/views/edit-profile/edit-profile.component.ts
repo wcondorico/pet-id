358bb631154acc06f972286e1244a384e7c14ca8
Alejandro Rodriguez <alex98rodriguezromero@gmail.com>
2024-11-30 01:06:24 -0500

    refactor: update edit profile

--- src/app/features/account/views/edit-profile/edit-profile.component.ts
+++ src/app/features/account/views/edit-profile/edit-profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@components/button/button.component';
 import { TitleComponent } from '@components/title/title.component';
+import { InputComponent } from '@components/input/input.component';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [ ButtonComponent, TitleComponent ],
+  imports: [ ButtonComponent, TitleComponent, InputComponent ],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.scss'
 })
