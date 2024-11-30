2b122421535287f5718316da95efbd271f290d25
Alejandro Rodriguez <alex98rodriguezromero@gmail.com>
2024-11-30 00:42:06 -0500

    refactor: update container for account, update button component

--- src/app/features/account/views/edit-profile/edit-profile.component.ts
+++ src/app/features/account/views/edit-profile/edit-profile.component.ts
@@ -1,13 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { IconButtonComponent } from '@components/icon-button/icon-button.component';
+import { Component } from '@angular/core';
+import { ButtonComponent } from '@components/button/button.component';
 import { TitleComponent } from '@components/title/title.component';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [IconButtonComponent, TitleComponent],
+  imports: [ ButtonComponent, TitleComponent ],
   templateUrl: './edit-profile.component.html',
-  styleUrl: './edit-profile.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './edit-profile.component.scss'
 })
 export class EditProfileComponent {
 
